69281b5caf5799c8681025339dc6623459ac5550
Carl Schroedl <cschroedl@usgs.gov>
2018-10-25 15:43:59 -0500

    Improving documentation for duplicate monitoring location validation.

--- src/main/java/gov/usgs/wma/mlrlegacy/validator/DuplicateMonitoringLocationValidator.java
+++ src/main/java/gov/usgs/wma/mlrlegacy/validator/DuplicateMonitoringLocationValidator.java
@@ -3,11 +3,19 @@ package gov.usgs.wma.mlrlegacy.validator;
 import gov.usgs.wma.mlrlegacy.MonitoringLocation;
 import gov.usgs.wma.mlrlegacy.UniqueKey;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Composite Constraint Validator. Validates that Monitoring Locations are not
  * duplicates in multiple ways.
+ * 
+ * To create and enforce a new ConstraintValidator for MonitoringLocations:
+ *  1) Create a new object that extends BaseDuplicateMonitoringLocationValidator
+ *  2) Add the new object to this class' autowired dependencies and its call to super(). 
+ *  3) Update the MyBatis mappings to support any new fields.
+ *  
  */
+@Component
 public class DuplicateMonitoringLocationValidator extends CompositeConstraintValidator<UniqueKey, MonitoringLocation>{
     
     @Autowired
@@ -20,5 +28,4 @@ public class DuplicateMonitoringLocationValidator extends CompositeConstraintVal
                 uniqueDistrictCodeAndStationNameValidator
         );
     }
-
 }
